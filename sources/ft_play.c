08ba99497599c03bb504aa1a7390f220627c9e73
Ana Isabel Neves <85195243+anineves@users.noreply.github.com>
2023-02-09 18:58:02 +0000

    Update ft_play.c

--- sources/ft_play.c
+++ sources/ft_play.c
@@ -1,5 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_play.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asousa-n <asousa-n@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/08 10:40:09 by asousa-n          #+#    #+#             */
+/*   Updated: 2023/02/08 13:41:14 by asousa-n         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "so_long.h"
 
+int	ft_win(t_game *game)
+{
+	ft_printf("Movements: %d\n", ++game->moves);
+	ft_free_all_allocated_memory(game);
+	ft_printf("CONGRATULATION, YOU WIN!!!! \n");
+	exit (EXIT_FAILURE);
+}
 
 void	ft_move_player(t_game *game, int n_y, int n_x, int player_sprite)
 {
@@ -7,11 +26,12 @@ void	ft_move_player(t_game *game, int n_y, int n_x, int player_sprite)
 	int	l_y;
 	
 	game->player_sprite = player_sprite;
+	ft_printf("exit x %d y %d\n ", game->map.p_exit.y, game->map.p_exit.x);
 	l_x = game->map.p_player.x;
 	l_y = game->map.p_player.y;
 	if (game->map.full[n_y][n_x] == MAP_EXIT && game->map.collectible == 0)
-		ft_victory(game);
-	else if ((game->map.full[n_y][n_x] == FLOOR) || (game->map.full[n_y][n_x] == COLLECTIBLE))
+		ft_win(game);
+	else if ((game->map.full[n_y][n_x] == FLOOR) || (game->map.full[n_y][n_x] == COLLECTIBLE) || (game->map.full[n_y][n_x] == MAP_EXIT))
 	{
 		game->map.full[l_y][l_x] = FLOOR;
 		if (game->map.full[n_y][n_x] == COLLECTIBLE)
@@ -19,11 +39,21 @@ void	ft_move_player(t_game *game, int n_y, int n_x, int player_sprite)
 		game->map.p_player.x = n_x;
 		game->map.p_player.y = n_y;
 		game->map.full[n_y][n_x] = PLAYER;
+		if (game->map.full[game->map.p_exit.y][game->map.p_exit.x] != PLAYER)
+			game->map.full[game->map.p_exit.y][game->map.p_exit.x] = MAP_EXIT;
 		game->moves++;
+		ft_printf("Moves: %d \n", game->moves);
 		ft_get_map(game);
 	}
 }
 
+int	ft_close(t_game *game)
+{
+	ft_printf("Movements: %d\n", game->moves);
+	ft_free_all_allocated_memory(game);
+	ft_printf("CLOSED\n");
+	exit (EXIT_FAILURE);
+}
 int	ft_play(int keysym, t_game *game)
 {
 	if (keysym == DOWN || keysym == S)
@@ -35,6 +65,6 @@ int	ft_play(int keysym, t_game *game)
 	if (keysym == RIGHT || keysym == D)
 		ft_move_player(game, game->map.p_player.y, game->map.p_player.x + 1, P_RIGHT);
 	if (keysym == ESC)
-		ft_close_game(game);
+		ft_close(game);
 	return (0);
 }
