ee43da1e072dbf74003555a39b04575c11a97319
Ana Isabel Neves <85195243+anineves@users.noreply.github.com>
2023-02-09 19:03:22 +0000

    Update so_long.h

--- sources/so_long.h
+++ sources/so_long.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asousa-n <asousa-n@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/08 10:40:59 by asousa-n          #+#    #+#             */
+/*   Updated: 2023/02/08 13:39:57 by asousa-n         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
@@ -8,33 +20,33 @@
 # include <X11/keysym.h>
 # include "../libft/libft.h"
 
-# define HEIGHT				64
-# define WIDTH				64
+# define HEIGHT			64
+# define WIDTH			64
 
-# define WALL				'1'
-# define FLOOR 				'0'
-# define COLLECTIBLE 			'C'
-# define PLAYER				'P'
-# define MAP_EXIT 		 	'E'
+# define WALL			'1'
+# define FLOOR 			'0'
+# define COLLECTIBLE 	'C'
+# define PLAYER			'P'
+# define MAP_EXIT 		'E'
 
 # define W				119
 # define A				97
 # define S				115
 # define D				100
-# define UP  				65362
-# define LEFT  				65361
-# define RIGHT 				65363
-# define DOWN	  			65364
+# define UP  			65362
+# define LEFT  			65361
+# define RIGHT 			65363
+# define DOWN	  		65364
 
-# define ESC  				65307
+# define ESC  			65307
 
-# define P_FRONT			1
-# define P_LEFT				2
-# define P_RIGHT			3
-# define P_BACK				4
+# define P_FRONT		1
+# define P_LEFT			2
+# define P_RIGHT		3
+# define P_BACK			4
 
-# define WALL_XPM			"sprites/wall.xpm"
-# define FLOOR_XPM			"sprites/floor.xpm"
+# define WALL_XPM				"sprites/wall.xpm"
+# define FLOOR_XPM				"sprites/floor.xpm"
 # define EXIT_CLOSED_XPM		"sprites/exit-closed.xpm"
 # define EXIT_OPEN_XPM			"sprites/exit-open.xpm"
 # define COLLECTIBLE_XPM		"sprites/collectible.xpm"
@@ -43,11 +55,6 @@
 # define PLAYER_LEFT_XPM		"sprites/player/left.xpm"
 # define PLAYER_RIGHT_XPM		"sprites/player/right.xpm"
 
-typedef enum e_bool
-{
-	false,
-	true
-}	t_bool;
 
 typedef struct s_position
 {
@@ -71,6 +78,7 @@ typedef struct s_map
 	int		exit;
 	int		player;
 	t_position	p_player;
+	t_position	p_exit;
 }	t_map;
 
 typedef struct s_game
@@ -80,7 +88,6 @@ typedef struct s_game
 	int		moves;
 	int		player_sprite;
 	t_map		map;
-	t_bool		map_alloc;
 	t_image		wall;
 	t_image		floor;
 	t_image		collectible;
@@ -110,14 +117,14 @@ void	ft_identify_sprite(t_game *game, int x, int y);
 void	ft_pos_player(t_game *game, int x, int y);
 void	ft_get_sprite(t_game *game, t_image sprite, int column, int line);
 void	ft_string_put(t_game *game);
-int	ft_play(int keysym, t_game *game);
+int		ft_play(int keysym, t_game *game);
 void	ft_move_player(t_game *game, int n_x, int n_y, int player_sprite);
-int	ft_victory(t_game *game);
-int	ft_close_game(t_game *game);
+int		ft_win(t_game *game);
+int		ft_close(t_game *game);
 void	ft_free_map(char **map);
 void	ft_free_all_allocated_memory(t_game *game);
 void	ft_destroy_images(t_game *game);
-int	ft_handler_error(char *msg, t_game *game);
+int		ft_handler_error(char *msg, t_game *game);
 void	ft_verific_path(t_game *game);
 
 
