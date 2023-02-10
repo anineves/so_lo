ac0b52b18a3923c751ef90a4ff56181c1d26244f
Ana Isabel Neves <85195243+anineves@users.noreply.github.com>
2023-02-10 18:53:56 +0000

    Update so_long.c

--- sources/so_long.c
+++ sources/so_long.c
@@ -3,9 +3,9 @@
 void	ft_verific_args(int argc, char **argv, t_game *game)
 {
 	if (argc != 2)
-		ft_handler_error("Invald number of arguments", game);
+		ft_handler_error1("Invald number of arguments", game);
 	if (*argv[1] == '\0')
-		ft_handler_error("Please choose a map", game);
+		ft_handler_error1("Please choose a map", game);
 }
 
 void	ft_verific_ber(char **argv, t_game *game)
@@ -14,7 +14,7 @@ void	ft_verific_ber(char **argv, t_game *game)
 
 	len_map = ft_strlen(argv[1]);
 	if (!ft_strnstr(&argv[1][len_map - 4], ".ber", 4))
-		ft_handler_error("The map must end in .ber", game);
+		ft_handler_error1("The map must end in .ber", game);
 }
 
 int main(int argc, char **argv)
@@ -37,7 +37,5 @@ int main(int argc, char **argv)
 	mlx_hook(game->win_ptr, 12, (1L<<15), ft_get_map, game);
 	mlx_loop(game->mlx_ptr);
 	ft_free_all_allocated_memory(game);
-	
-
 
 }
